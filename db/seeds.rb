2029b53c2c5deb84730f28e1b84cf71c35b798f6
jackie kircher <kircher.jackie@gmail.com>
2015-03-05 17:19:50 -0500

    make a seperate white list file that's easy to read
    the primary points of db/account_list.txt and db/global_whitelist.txt are
    to provide a quick file-view of what accounts are known of and for
    seeding the database on a new fork. they also provide an easy place to
    make pull requests to add new accounts to either.
    
    because of this last functionality, db/seeds.rb has been updated so that
    the databased can be re-seeded at any point and not create a ton of
    duplicates, and also to update the whitelist status of accounts that
    already in it.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,22 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # seed in the list of _ebooks accounts on record
-account_file = Rails.root + "db/account_list.txt"
-File.open(account_file, 'r') do |file|
-  file.each do |line|
-    begin
-      account = line.split(":")
-      EbookAccount.create!(
-        twitter_id:  account[0].strip.to_i,
-        screen_name: account[1].strip
-      )
-    rescue ActiveRecord::RecordNotSaved
-      puts "bullshit!"
-      # I should do something real here
+lists = [[ Rails.root + "db/account_list.txt",     false ],
+         [ Rails.root + "db/global_whitelist.txt", true  ]]
+
+lists.each do |list, whitelisted|
+  File.open(list, 'r') do |file|
+    file.each do |line|
+      line    = line.split(":")
+      account = { twitter_id:  line[0].strip.to_i,
+                  screen_name: line[1].strip,
+                  whitelisted: whitelisted }
+      begin
+        EbookAccount.create!(account)
+      rescue ActiveRecord::RecordInvalid
+        account = EbookAccount.where(twitter_id: account[:twitter_id]).first
+        account.update(whitelisted: account[:whitelisted])
+      end
     end
   end
 end
